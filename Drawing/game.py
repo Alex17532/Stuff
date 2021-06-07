0e26a863b4b5426fce2568fd8e11d44ba24e6f77
Alex17532 <0alexlg12@gmail.com>
2021-06-07 17:00:59 +0100

    main

--- Drawing/game.py
+++ Drawing/game.py
@@ -1,6 +1,12 @@
 import turtle
+import random
 
-pen = turtle.turtle()
+pen = turtle.Turtle()
+
+x = pen.xcor()
+y = pen.ycor()
 
 for i in range(94):
-    
+    x += 10
+    y += 10
+    pen.seth(random.randint(90, 180))
