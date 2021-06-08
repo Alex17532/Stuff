45ca422526deff888bf4784392a2c44c0d0c40d1
Alex17532 <0alexlg12@gmail.com>
2021-06-08 17:29:16 +0100

    main

--- Drawing/game.py
+++ Drawing/game.py
@@ -1,12 +1,14 @@
 import turtle, random
 
+wn_names = [". dRaWiNg GaMe •", "• DrAwInG gAmE ."]
+
 wn = turtle.Screen()
 wn.title("Drawing game")
 wn.bgcolor("black")
 
 pen = turtle.Turtle()
 pen.speed(0)
-pen.shape("circle")
+pen.shape("square")
 pen.color("#66ffff")
 
 def follow_cursor(x, y):
@@ -16,5 +18,6 @@ wn.onclick(follow_cursor)
 
 while True:
     pen.seth(random.randint(0, 360))
+    wn.title(random.choice(wn_names))
 
 wn.mainloop()
