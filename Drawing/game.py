8c4f6ec185f7c6598953c3d618e3434e48a2fd72
Alex17532 <0alexlg12@gmail.com>
2021-06-12 14:40:19 +0100

    Inital commit

--- Drawing/game.py
+++ Drawing/game.py
@@ -1,5 +1,7 @@
 import turtle, random
 
+running = True
+
 wn_names = [". dRaWiNg GaMe °", "° DrAwInG gAmE ."]
 colours = ["yellow", "gold", "orange", "red", "maroon", "violet", "magenta", "purple", "navy", "blue", "skyblue", "cyan", 'turquoise', 'lightgreen', 'green', 'darkgreen', 'chocolate', 'brown', 'gray', 'white']
 
@@ -20,20 +22,25 @@ def draw_square():
         pen.forward(100)
         pen.right(90)
 
+def close_window():
+    global running
+    running = False
+    wn._destroy()
+
 def clear_drawing():
     pen.clear()
 
 
 
 wn.listen()
-wn.onkeypress(draw_square, "q")
+wn.onkeypress(draw_square, "s")
 wn.onkeypress(clear_drawing, "c")
+wn.onkeypress(close_window, "q")
 
 wn.onclick(draw)
 
-
-while True:
-    # draw_square()
+while running:
+    draw_square()
     pen.color(random.choice(colours))
     pen.seth(random.randint(0, 360))
     wn.title(random.choice(wn_names))
