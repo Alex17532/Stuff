d74e287a0ed52797f7a74fc32c59fa7f66d5d6f9
Alex17532 <0alexlg12@gmail.com>
2021-06-14 08:58:48 +0100

    Initial commit

--- Drawing/game.py
+++ Drawing/game.py
@@ -3,8 +3,9 @@ import turtle, random
 
 running = True
 
-wn_names = ["Drawing Game - Fullscreen bugs the drawing so don't use it"]
+wn_name = ["{Drawing Game}"]
 colours = ["yellow", "gold", "orange", "red", "maroon", "violet", "magenta", "purple", "navy", "blue", "skyblue", "cyan", 'turquoise', 'lightgreen', 'green', 'darkgreen', 'chocolate', 'brown', 'gray', 'white']
+shapes = ["square", "arrow", "circle", "turtle", "triangle", "classic"]
 
 class Main():
     def __init__(self):
@@ -16,7 +17,7 @@ class Main():
         self.y = 300
         self.t.speed(0)
         wn.bgcolor("black")
-        wn.title(random.choice(wn_names))
+        wn.title(wn_name)
         screen.bind('<Motion>', self.set_coords)
         self.run()
     def set_coords(self, event):
@@ -29,7 +30,7 @@ class Main():
                 print(event.x, event.y)
                 pen.goto(event.x, event.y)
             self.t.seth(random.randint(1, 360))
-            self.t.shape("triangle")
+            self.t.shape(random.choice(shapes))
             self.t.color(random.choice(colours))
             self.t.setposition(self.x-300, (self.y*-1)+300)
 m = Main()
