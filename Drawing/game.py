ff4fd9a268617f1777204aa2fba420ddda1ebca0
Alex17532 <0alexlg12@gmail.com>
2021-06-14 08:55:17 +0100

    Initial commit

--- Drawing/game.py
+++ Drawing/game.py
@@ -3,46 +3,33 @@ import turtle, random
 
 running = True
 
-wn_names = [". dRaWiNg GaMe °", "° DrAwInG gAmE ."]
+wn_names = ["Drawing Game - Fullscreen bugs the drawing so don't use it"]
 colours = ["yellow", "gold", "orange", "red", "maroon", "violet", "magenta", "purple", "navy", "blue", "skyblue", "cyan", 'turquoise', 'lightgreen', 'green', 'darkgreen', 'chocolate', 'brown', 'gray', 'white']
 
-wn = turtle.Screen()
-wn.title("Drawing game")
-wn.bgcolor("black")
-
-pen = turtle.Turtle()
-pen.speed(0)
-pen.shape("square")
-pen.color(random.choice(colours))
-
-def draw(x, y):
-    pen.goto(x, y)
-
-def draw_square():
-    for i in range(4):
-        pen.forward(100)
-        pen.right(90)
-
-def close_window():
-    global running
-    running = False
-    wn._destroy()
-
-def clear_drawing():
-    pen.clear()
-
-
-wn.listen()
-wn.onkeypress(draw_square, "s")
-wn.onkeypress(clear_drawing, "c")
-wn.onkeypress(close_window, "q")
-
-wn.onclick(draw)
-
-while running:
-    draw_square()
-    pen.color(random.choice(colours))
-    pen.seth(random.randint(0, 360))
-    wn.title(random.choice(wn_names))
-
-wn.mainloop()
+class Main():
+    def __init__(self):
+        wn = turtle.Screen()
+        wn.setup(600, 600)
+        screen = wn.getcanvas()
+        self.t = turtle.Turtle()
+        self.x = 300
+        self.y = 300
+        self.t.speed(0)
+        wn.bgcolor("black")
+        wn.title(random.choice(wn_names))
+        screen.bind('<Motion>', self.set_coords)
+        self.run()
+    def set_coords(self, event):
+        print(event.x, event.y)
+        self.x = event.x
+        self.y = event.y
+    def run(self):
+        while True:
+            def func(event):
+                print(event.x, event.y)
+                pen.goto(event.x, event.y)
+            self.t.seth(random.randint(1, 360))
+            self.t.shape("triangle")
+            self.t.color(random.choice(colours))
+            self.t.setposition(self.x-300, (self.y*-1)+300)
+m = Main()
