deae2f16df33b7aa0cc9cc7ae3149f18753f1bbb
Alex17532 <0alexlg12@gmail.com>
2021-06-14 14:41:42 +0100

    Initial commit

--- Drawing/game.py
+++ Drawing/game.py
@@ -26,8 +26,9 @@ class Main():
     def run(self):
         while True:
             self.t.left(540540540540540)
-            self.t.home()
-            self.t.forward(190)
+            # self.t.home()
+            # self.t.forward(190)
+            self.t.circle(10)
             def func(event):
                 print(event.x, event.y)
                 pen.goto(event.x, event.y)
