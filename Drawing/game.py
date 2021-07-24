abb7fe2305aa7f6409bdc0fa2abb1a4598d6c9d9
Alex17532 <0alexlg12@gmail.com>
2021-07-24 13:35:05 +0100

    Inital commit

--- Drawing/game.py
+++ Drawing/game.py
@@ -7,12 +7,15 @@ class Main():
         wn.setup(600, 600)
         screen = wn.getcanvas()
         self.t = turtle.Turtle()
-        self.t.shapesize(stretch_wid=1, stretch_len=1, outline=5)
+        self.t.shapesize(stretch_wid=1, stretch_len=1, outline=2)
         self.x = 300
         self.y = 300
         self.t.speed(0)
+        self.t.color("green")
+        self.t.shape("triangle")
+        self.t.setheading(135)
+        self.t.pensize(5)
         wn.bgcolor("black")
-        wn.title(random.choice(wn_name))
         screen.bind('<Motion>', self.set_coords)
         self.run()
     def set_coords(self, event):
